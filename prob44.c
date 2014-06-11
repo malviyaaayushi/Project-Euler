9872208afc32a8c4b0fae45850bca04afa037b5b
malviyaaayushi <malviyaaayushi@gmail.com>
2014-06-11 19:58:22 +0530

    Update prob44.c

--- prob44.c
+++ prob44.c
@@ -13,7 +13,7 @@ int isPentagonal(uint64_t n){
 
 int main(){
 
-    uint64_t i = 0, j = 0, num1 = 0, num2 = 0;
+    uint64_t i = 0, j = 1, num1 = 0, num2 = 0, num3 = 0;
     int found = 0;
     //printf("%d", isPentagonal(22));
     while(!found){
@@ -21,14 +21,14 @@ int main(){
         num1 = i * (3*i - 1)/2;
         for(j=1; j<i; j++){
             num2 = j * (3*j - 1)/2;
-            num1 = num1 + num2;
-            num2 = num1 - 2*num2;
-            printf("%d %d\n", i, j);
+            num3 = num1 + num2;
+            num2 = num1 - num2;
+            //printf("%" PRIu64 " ""%" PRIu64 " ""\n", num3, num2);
             if(num2 < 0){
                 num2 *= -1;
             }
-            if(isPentagonal(num1) && isPentagonal(num2)){
-                printf("ans = %d\n", num2);
+            if(isPentagonal(num3) && isPentagonal(num2)){
+                printf("ans = ""%" PRIu64 "\n", num2);
                 found = 1;
                 break;
             }
